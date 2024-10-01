f717ba53cf99347768c52df326284d803c2b9dde
ChuMinhDuc <chuminhduc4623@gmail.com>
2024-10-01 20:02:18 +0700

    UPDATE REPOSITORY

---
+++ api/Interfaces/IStockRepository.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Models;
+using api.Repository;
+
+namespace api.Interfaces
+{
+    public interface IStockRepository
+    {
+  
+        // Thêm nhiệm vụ
+        Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
+        Task<Stock?> DeleteAsync(int id);
+    }
+}
