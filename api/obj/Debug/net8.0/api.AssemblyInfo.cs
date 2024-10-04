f40b2ec3ad16ec882c5a70cd9f0ccbd02fdcbfe0
ChuMinhDuc <chuminhduc4623@gmail.com>
2024-10-05 00:29:02 +0700

    Update Sorting

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ccb800a292e7f03cef2356ca750bc1b1aaf240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174f3507975bf5d1fae134aee0197eaf53dd799d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
