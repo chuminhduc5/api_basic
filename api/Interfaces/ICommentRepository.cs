8dd0a54211b39677afd7b73a6347ae37c1709f59
ChuMinhDuc <chuminhduc4623@gmail.com>
2024-10-04 15:13:14 +0700

    Update Post and Delete

--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -14,5 +14,9 @@ namespace api.Interfaces
         Task<Comment?> GetByIdAsync(int id);
 
         Task<Comment> CreateAsync(Comment commentModel);
+
+        Task<Comment?> UpdateAsync(int id, Comment commentModel);
+
+        Task<Comment?> DeleteAsync(int id);
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8862be43ec84eebea77694bcc8650de10aa9a44a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9968acce822e29cf583894185de43a9685c305d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
