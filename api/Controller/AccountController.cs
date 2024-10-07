f555336b2f900edc722a89a2d33de68e49f124e0
ChuMinhDuc <chuminhduc4623@gmail.com>
2024-10-07 08:51:02 +0700

    Update Login

--- api/Controller/AccountController.cs
+++ api/Controller/AccountController.cs
@@ -3,6 +3,7 @@ using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controller;
 
@@ -12,12 +13,45 @@ public class AccountController : ControllerBase
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly ITokenService _tokenService;
+    private readonly SignInManager<AppUser> _signInManger;
     // Trình quản lý người dùng
-    public AccountController(UserManager<AppUser> userManger, ITokenService tokenService)
+    public AccountController(UserManager<AppUser> userManger, ITokenService tokenService, SignInManager<AppUser> signInManager)
     {
         _userManager = userManger;
         _tokenService = tokenService;
+        _signInManger = signInManager;
     }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto loginDto)
+    {
+        if(!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = await _userManager.Users.FirstOrDefaultAsync(
+            x => x.UserName == loginDto.Username.ToLower()
+        );
+
+        // Trường hợp nếu không tìm thấy người dùng
+        if(user == null) return Unauthorized("Invalid UserName!");
+
+        var result = await _signInManger.CheckPasswordSignInAsync(user, loginDto.Password, false);
+        
+        // Trường hợp đăng nhập không thành công thành công
+        if(!result.Succeeded) return Unauthorized("Username not found and/or password incorrect");
+
+        return Ok(
+            new NewUserDto
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+            }
+        );
+    }
+
 
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d98ab1edc718348efab813e18249dbd8c940f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f7b119ecf6931c9ef78e09d2e4a24ad86d95e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
